7ae3fd48d37dac15ded0e5d21fafb5247b1bea99
fluorjo <fluorjo@naver.com>
2022-09-12 20:49:42 +0900

    0912_정처기_실기_21.1_java07번

---
+++ src/a21107.java
@@ -0,0 +1,12 @@
+public class a21107 {
+    public static void main(String[] args){
+        int a[][]={{45,50,75},{89}};
+        //a[0]=앞의 숫자 3개 배열, a[1]=뒤의 89 배열.
+        System.out.println(a[0].length);
+        //a[0]의 길이이므로 3.
+        System.out.println(a[1].length);        
+        System.out.println(a[0][0]);
+        System.out.println(a[0][1]);
+        System.out.println(a[1][0]);        
+    }
+}
