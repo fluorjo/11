eeca84c86f6ff3297f2c6f8bf7d6862f892b7544
fluorjo <fluorjo@naver.com>
2022-09-13 04:32:35 +0900

    0913_정처기_실기_21.1_java18

---
+++ src/a21118.java
@@ -0,0 +1,16 @@
+public class a21118 {
+    public static void main(String[]args) {
+        int i,j;
+        for(j=0,i=0; i<=5; i++){
+            j+=i;
+            System.out.print(i);
+            if(i==5){
+                System.out.print("=");
+                System.out.print(j);
+            }else{
+                System.out.print("+");
+            }
+        }
+    }
+}
+
